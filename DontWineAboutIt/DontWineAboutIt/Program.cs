6c7a335d0296140e27d82710fb4efd2758d46457
Matt <wrestler.411@comcast.net>
2018-01-10 09:09:51 -0800

    Initial Commit

---
+++ DontWineAboutIt/DontWineAboutIt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DontWineAboutIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
